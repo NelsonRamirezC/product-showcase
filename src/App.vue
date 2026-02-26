162be082da76ce59c65ec2ebf71a681b8b739aa9
Nelson <xhyskoax@gmail.com>
2026-02-26 11:21:32 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Product ShowCase</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <RouterLink to="/" class="nav-link">Inicio</RouterLink>
+          <RouterLink to="/products" class="nav-link">Productos</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <RouterView />
+
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+
+<style scoped>
+.router-link-active,
+.router-link-exact-active {
+  font-weight: 600;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <HeaderComp>Página Home</HeaderComp>
+
+  </div>
+</template>
+
+<script setup>
+import HeaderComp from '@/components/layouts/HeaderComp.vue';
+
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
